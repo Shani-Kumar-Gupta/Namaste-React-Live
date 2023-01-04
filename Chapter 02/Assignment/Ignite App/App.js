5f20154e2cb301111bb15982c553fa6be6aeacd2
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2023-01-04 22:17:06 +0530

    🚀 create-react-app from scratch | used parcel bundler (For production ready build) | used .gitignore and browserslist package

---
+++ Chapter 02/Assignment/Ignite App/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading1 = React.createElement(
+  'h1',
+  {
+    style: {
+      color: 'white',
+    },
+    key: 'title',
+  },
+  'Namaste React'
+);
+
+const heading2 = React.createElement(
+  'h2',
+  {
+    id: 'heading2',
+    className: 'haeding2',
+    key: 'heading2',
+  },
+  'Shani This Side 😊 Happy Learning React!'
+);
+
+const container = React.createElement(
+  'div',
+  {
+    style: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      width: '100%',
+      height: '100vh',
+      fontSize: '30px',
+      backgroundColor: 'lightgreen',
+    },
+  },
+  [heading1, heading2]
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container);
