63b96c53d26e2bd81ca2876c2574ebc7533e75c8
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2023-01-06 22:24:01 +0530

    🚀 JSX | How JSX works and executes | Advantages

--- Chapter 03/foundation/App.js
+++ Chapter 03/foundation/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const heading = React.createElement('h1', {}, 'Hello Devs');
+// How React.createElement get's executed?
 // React.createElement => Returns an Object => Converted into HTML through React => Reander in the DOM (Using ReactDOM)
 
 // JSX Syntax =>
@@ -10,6 +11,17 @@ const heading = React.createElement('h1', {}, 'Hello Devs');
 // Question: Is JSX html inside JavaScript is True or False? => True :( => False :)
 // This (JSX) is html like syntax => Fancy way of writing HTML
 // How JavaScript recognizes this syntax?????
+// How does this work in browsers?
+
+// How JSX executes behind the scenes?
+// JSX (babel) => React.createElement => Returns an Object => Converted into HTML through React => Reander in the DOM (Using ReactDOM)
+/*
+Advantages of JSX:
+  Readibility
+  Mantainability
+  Developer friendly
+  Clean and less code
+*/
 const heading1 = (
   <h1 id="hello" key={1}>
     Namaste Devs
@@ -20,4 +32,4 @@ const div = React.createElement('div', {}, [heading, heading1]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(div);
+root.render(heading1);
