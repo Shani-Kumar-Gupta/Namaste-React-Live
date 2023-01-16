4218e3244d16d2bae96e5a8302595bd732efdc2c
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-01-16 23:55:00 +0530

    🚀 App Layout added

--- Food Villa/App.js
+++ Food Villa/App.js
@@ -7,6 +7,32 @@ const Title = () => (
   </h1>
 );
 
+/* Step 1: Build your App Layout */
+const AppLayout = () => {
+  return (
+    {
+      /*
+      Header
+        - Logo
+        - Nav Bar List Items (Right Side)
+        - Cart
+      Body
+        - Search Bar
+        - Restraunt List
+          - Restraunt Card
+            - Image
+            - Name
+            - Rating
+            - Cusines List
+      Footer
+        - Ref Links
+        - Copyright
+        - Address
+      */
+    }
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<Title />);
