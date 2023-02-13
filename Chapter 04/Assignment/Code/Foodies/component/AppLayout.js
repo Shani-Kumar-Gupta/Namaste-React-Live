6be96f1b038265ae1eea11ce9a52686080512215
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-02-13 22:47:38 +0530

    🚀 Config driven UI example

--- Chapter 04/Assignment/Code/Foodies/component/AppLayout.js
+++ Chapter 04/Assignment/Code/Foodies/component/AppLayout.js
@@ -44,6 +44,40 @@ const FooterComponent = () => {
   )
 }
 
+/*
+Config Driven UI: Config driven UI is a way of dynamically handle our UI through config which we will get from some backend API.
+                  Whole UI is controled by a configuration which will receive from some backend API.
+                  Control UI through backend API.
+                  Based on config data which will receive from some backend API, UI will get rendered.
+*/
+
+const config = [
+  {
+    type: "carousel",
+    cards: [
+      {
+        imageURL: "......",
+        offer: "50% Off",
+      },
+      {
+        imageURL: ".......",
+        offer: "No Delivery Charges"
+      }
+    ]
+  },
+  {
+    type: "restaurants",
+    cards: [
+      {
+        name: "Burger King",
+      },
+      {
+        name: "KFC"
+      }
+    ]
+  }
+]
+
 const AppLayout = () => {
   return (
     <React.Fragment>
