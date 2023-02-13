4df35454ba67222801d3a8c0767125b105f14eb4
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-02-13 22:38:04 +0530

    🚀 Foodies: Global font-family and Basic component

--- Chapter 04/Assignment/Code/Foodies/component/AppLayout.js
+++ Chapter 04/Assignment/Code/Foodies/component/AppLayout.js
@@ -1,9 +1,56 @@
 import React from 'react';
 
+/***** Project Planning *******/
+/*
+  Header Component
+    Logo and Title
+    Navigation Item
+      Home
+      About Us
+      Contact
+      Sign in
+      Offers
+      Support
+      Cart
+  Body Component
+    Search Bar
+    Restaurant Card
+      Image
+      Description
+      Prize
+      Rating
+      Cuisines
+  Footer Component
+    Reference Link
+    Copyright
+    Address
+*/
+
+const HeaderComponent = () => {
+  return (
+    <div>Header Component</div>
+  )
+}
+
+const BodyComponent = () => {
+  return (
+    <div>Body Component</div>
+  )
+}
+
+const FooterComponent = () => {
+  return (
+    <div>Footer Component</div>
+  )
+}
+
 const AppLayout = () => {
   return (
     <React.Fragment>
       <div>Hii AppLayout</div>
+      <HeaderComponent />
+      <BodyComponent />
+      <FooterComponent />
     </React.Fragment>
   )
 }
