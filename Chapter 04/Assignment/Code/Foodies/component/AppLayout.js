3c178c9035d714a679d5f46501acdd9bc0a1ed0f
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-02-13 22:29:10 +0530

    🚀 Chapter 04: Food Ordering Applcation - Project setup and configuration done

---
+++ Chapter 04/Assignment/Code/Foodies/component/AppLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppLayout = () => {
+  return (
+    <React.Fragment>
+      <div>Hii AppLayout</div>
+    </React.Fragment>
+  )
+}
+
+export default AppLayout;
