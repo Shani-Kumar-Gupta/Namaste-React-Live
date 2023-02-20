f91756846e6f656e24b2a602183f117db112f059
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2023-02-20 23:57:32 +0530

    🚀 React props - different ways of passing and receiving props

--- Chapter 04/Assignment/Code/Foodies/component/AppLayout.js
+++ Chapter 04/Assignment/Code/Foodies/component/AppLayout.js
@@ -34,8 +34,8 @@ const Title = () => {
         <img alt="foodies logo" src={images.logo} className="logo" />
       </a>
     </div>
-  )
-}
+  );
+};
 
 const HeaderComponent = () => {
   return (
@@ -50,44 +50,148 @@ const HeaderComponent = () => {
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
 /*
 1. Currently my all restaurants cards having same data and some how we have to pass some dynamic data to each card -> props as an attibute
 */
-const RestaurantCard = () => {
-  let imgURL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + RestaurantData?.[0]?.data?.cloudinaryImageId;
+/* Mapping Static Data */
+// const RestaurantCard = () => {
+//   let imgURL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + RestaurantData?.[0]?.data?.cloudinaryImageId;
+//   return (
+//     <div className="restaurant__card">
+//       <img alt={RestaurantData?.[0]?.data?.name} src={imgURL} className="cardImg" />
+//       <h2>{RestaurantData?.[0]?.data?.name}</h2>
+//       <h3>{RestaurantData?.[0]?.data?.cuisines?.join(", ") }</h3>
+//       <h4>{RestaurantData?.[0]?.data?.lastMileTravelString }</h4>
+//     </div>
+//   )
+// }
+
+/* Mapping dynamic data - passing as props */
+
+// const RestaurantCard = (props) => {
+//   console.log("my props", props);
+//   let imgURL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + props?.restaurantDetails?.data?.cloudinaryImageId;
+//   return (
+//     <div className="restaurant__card">
+//       <img alt={props?.restaurantDetails?.data?.name} src={imgURL} className="cardImg" />
+//       <h2>{props?.restaurantDetails?.data?.name}</h2>
+//       <h3>{props?.restaurantDetails?.data?.cuisines?.join(", ") }</h3>
+//       <h4>{props?.restaurantDetails?.data?.lastMileTravelString }</h4>
+//     </div>
+//   )
+// }
+
+/* Receive props as object destructure */
+// const RestaurantCard = ({restaurantDetails}) => {
+//   console.log("my props", restaurantDetails);
+//   const {name, cuisines, lastMileTravelString, cloudinaryImageId} = restaurantDetails;
+//   let imgURL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + restaurantDetails?.data?.cloudinaryImageId;
+//   return (
+//     <div className="restaurant__card">
+//       <img alt={restaurantDetails?.data?.name} src={imgURL} className="cardImg" />
+//       <h2>{restaurantDetails?.data?.name}</h2>
+//       <h3>{restaurantDetails?.data?.cuisines?.join(", ") }</h3>
+//       <h4>{restaurantDetails?.data?.lastMileTravelString }</h4>
+//     </div>
+//   )
+// }
+
+// const RestaurantCard = ({restaurantDetails}) => {
+//   console.log("my props", restaurantDetails);
+//   const {name, cuisines, lastMileTravelString, cloudinaryImageId} = restaurantDetails?.data;
+//   let imgURL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + cloudinaryImageId;
+//   return (
+//     <div className="restaurant__card">
+//       <img alt={name} src={imgURL} className="cardImg" />
+//       <h2>{name}</h2>
+//       <h3>{cuisines?.join(", ") }</h3>
+//       <h4>{lastMileTravelString }</h4>
+//     </div>
+//   )
+// }
+
+const RestaurantCard = ({
+  name,
+  cuisines,
+  lastMileTravelString,
+  cloudinaryImageId,
+}) => {
+  let imgURL =
+    'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/' +
+    cloudinaryImageId;
   return (
     <div className="restaurant__card">
-      <img alt={RestaurantData?.[0]?.data?.name} src={imgURL} className="cardImg" />
-      <h2>{RestaurantData?.[0]?.data?.name}</h2>
-      <h3>{RestaurantData?.[0]?.data?.cuisines?.join(", ") }</h3>
-      <h4>{RestaurantData?.[0]?.data?.lastMileTravelString }</h4>
+      <img alt={name} src={imgURL} className="cardImg" />
+      <h2>{name}</h2>
+      <h3>{cuisines?.join(', ')}</h3>
+      <h4>{lastMileTravelString}</h4>
     </div>
-  )
-}
+  );
+};
 
 /*
 props - properties (This is way of passing data to the component using props)
+        passing data from parent component to child component
 */
+
+// const BodyComponent = () => {
+//   return (
+//     <div className="body__container">
+//       {/* {RestaurantCard(RestaurantData[0])} */}
+//       <RestaurantCard restaurantDetails={RestaurantData[0]} /> {/* Passing restaurantDetails as props - look like an html attribute */}
+//       <RestaurantCard restaurantDetails={RestaurantData[1]} />
+//       <RestaurantCard restaurantDetails={RestaurantData[2]} />
+//       <RestaurantCard restaurantDetails={RestaurantData[3]} />
+//     </div>
+//   )
+// }
+
+// const BodyComponent = () => {
+//   return (
+//     <div className="body__container">
+//       {/* {RestaurantCard(RestaurantData[0])} */}
+//       <RestaurantCard
+//         name={RestaurantData[0]?.data?.name}
+//         cuisines={RestaurantData[0]?.data?.cuisines}
+//         lastMileTravelString={RestaurantData[0]?.data?.lastMileTravelString}
+//         cloudinaryImageId={RestaurantData[0]?.data?.cloudinaryImageId}
+//       />{' '}
+//       {/* Passing restaurantDetails as props - look like an html attribute */}
+//       <RestaurantCard
+//         name={RestaurantData[1]?.data?.name}
+//         cuisines={RestaurantData[1]?.data?.cuisines}
+//         lastMileTravelString={RestaurantData[1]?.data?.lastMileTravelString}
+//         cloudinaryImageId={RestaurantData[1]?.data?.cloudinaryImageId}
+//       />
+//       <RestaurantCard
+//         name={RestaurantData[2]?.data?.name}
+//         cuisines={RestaurantData[2]?.data?.cuisines}
+//         lastMileTravelString={RestaurantData[2]?.data?.lastMileTravelString}
+//         cloudinaryImageId={RestaurantData[2]?.data?.cloudinaryImageId}
+//       />
+//       <RestaurantCard
+//         {...RestaurantData[3]?.data}
+//       />
+//     </div>
+//   );
+// };
 
 const BodyComponent = () => {
   return (
     <div className="body__container">
-      <RestaurantCard restaurantDetails={RestaurantData[0]} /> {/* Passing restaurantDetails as props - look like an html attribute */}
-      <RestaurantCard restaurantDetails={RestaurantData[1]} />
-      <RestaurantCard restaurantDetails={RestaurantData[2]} />
-      <RestaurantCard restaurantDetails={RestaurantData[3]} />
+      {RestaurantData.map((restaurant) => {
+        return <RestaurantCard key={restaurant?.data?.id} {...restaurant?.data}/>
+      })}
     </div>
-  )
-}
+  );
+};
 
 const FooterComponent = () => {
-  return (
-    <div>Footer Component</div>
-  )
-}
+  return <div>Footer Component</div>;
+};
 
 /*
 Config Driven UI: Config driven UI is a way of dynamically handle our UI through config which we will get from some backend API.
@@ -98,30 +202,30 @@ Config Driven UI: Config driven UI is a way of dynamically handle our UI through
 
 const config = [
   {
-    type: "carousel",
+    type: 'carousel',
     cards: [
       {
-        imageURL: "......",
-        offer: "50% Off",
+        imageURL: '......',
+        offer: '50% Off',
       },
       {
-        imageURL: ".......",
-        offer: "No Delivery Charges"
-      }
-    ]
+        imageURL: '.......',
+        offer: 'No Delivery Charges',
+      },
+    ],
   },
   {
-    type: "restaurants",
+    type: 'restaurants',
     cards: [
       {
-        name: "Burger King",
+        name: 'Burger King',
       },
       {
-        name: "KFC"
-      }
-    ]
-  }
-]
+        name: 'KFC',
+      },
+    ],
+  },
+];
 
 const AppLayout = () => {
   return (
@@ -130,7 +234,7 @@ const AppLayout = () => {
       <BodyComponent />
       <FooterComponent />
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default AppLayout;
--- Chapter 04/Assignment/Code/Foodies/constants/data/RestaurantData.js
+++ Chapter 04/Assignment/Code/Foodies/constants/data/RestaurantData.js
@@ -558,6 +558,566 @@ const RestaurantData = [
       "new": false
     },
     "subtype": "basic"
+  },
+  {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "639592",
+      "name": "La Pino'z Pizza",
+      "uuid": "840d2458-e656-465c-81e4-ca129b1363a3",
+      "city": "2",
+      "area": "Sector 37",
+      "totalRatingsString": "100+ ratings",
+      "cloudinaryImageId": "zupxsbhy3vx3vvdaibnw",
+      "cuisines": [
+        "Pizzas",
+        "Italian",
+        "Pastas",
+        "Mexican",
+        "Desserts"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 40000,
+      "costForTwoString": "₹400 FOR TWO",
+      "deliveryTime": 34,
+      "minDeliveryTime": 34,
+      "maxDeliveryTime": 34,
+      "slaString": "34 MINS",
+      "lastMileTravel": 5.5,
+      "slugs": {
+        "restaurant": "la-pinoz-pizza-khandsa-marg-old-gurgaon-zone-6",
+        "city": "gurgaon"
+      },
+      "cityState": "2",
+      "address": "S.C.O- 16, GROUND AND BASEMENT, KHANDSA MARG, SEC-10 A. GURUGRAM , PIN- 122001",
+      "locality": "Khandsa Marg",
+      "parentId": 4961,
+      "unserviceable": false,
+      "veg": false,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "aggregatedDiscountInfo": {
+        "header": "40% off",
+        "shortDescriptionList": [
+          {
+            "meta": "40% off | Use SWIGGYIT",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "40% off up to ₹80 | Use code SWIGGYIT",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "aggregatedDiscountInfoV2": {
+        "header": "40% OFF",
+        "shortDescriptionList": [
+          {
+            "meta": "Use SWIGGYIT",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "40% off up to ₹80 | Use code SWIGGYIT",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "ribbon": [
+        {
+          "type": "PROMOTED"
+        }
+      ],
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          
+        ],
+        "totalFees": 0,
+        "message": "",
+        "title": "",
+        "amount": "",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 0,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "cid=5938986~p=1~eid=00000186-4bde-b2fe-3ce0-21b600ad0152",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "5.5 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "639591",
+        "deliveryTime": 34,
+        "minDeliveryTime": 34,
+        "maxDeliveryTime": 34,
+        "lastMileTravel": 5.5,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "NOT_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": true,
+      "avgRating": "3.7",
+      "totalRatings": 100,
+      "new": false
+    },
+    "subtype": "basic"
+  },
+  {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "154654",
+      "name": "Jagdish Vaishno Dhaba",
+      "uuid": "a783085e-830a-4a33-8831-1274bd944def",
+      "city": "2",
+      "area": "Old Railway Road",
+      "totalRatingsString": "1000+ ratings",
+      "cloudinaryImageId": "seyuhw8itjtlmvm7wscy",
+      "cuisines": [
+        "Indian",
+        "North Indian",
+        "Biryani",
+        "Tandoor",
+        "Punjabi"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 25000,
+      "costForTwoString": "₹250 FOR TWO",
+      "deliveryTime": 20,
+      "minDeliveryTime": 20,
+      "maxDeliveryTime": 20,
+      "slaString": "20 MINS",
+      "lastMileTravel": 2.200000047683716,
+      "slugs": {
+        "restaurant": "jagdish-vaishno-dhaba-old-gurgaon-zone-6",
+        "city": "gurgaon"
+      },
+      "cityState": "2",
+      "address": "JAGDISH VAISHNO DHABA OPP Gurgaon Dreams Mall ",
+      "locality": "Sector 4",
+      "parentId": 15300,
+      "unserviceable": false,
+      "veg": true,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "aggregatedDiscountInfo": {
+        "header": "50% off",
+        "shortDescriptionList": [
+          {
+            "meta": "50% off | Use GUILTFREE",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "50% off up to ₹120 | Use code GUILTFREE",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "aggregatedDiscountInfoV2": {
+        "header": "50% OFF",
+        "shortDescriptionList": [
+          {
+            "meta": "Use GUILTFREE",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "50% off up to ₹120 | Use code GUILTFREE",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          
+        ],
+        "totalFees": 0,
+        "message": "",
+        "title": "",
+        "amount": "",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 0,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "2.2 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "154653",
+        "deliveryTime": 20,
+        "minDeliveryTime": 20,
+        "maxDeliveryTime": 20,
+        "lastMileTravel": 2.200000047683716,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "NOT_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": false,
+      "avgRating": "4.2",
+      "totalRatings": 1000,
+      "new": false
+    },
+    "subtype": "basic"
+  },
+  {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "254006",
+      "name": "McDonald's",
+      "uuid": "f4398cc4-6dcd-4969-8715-8ce1f5fbeccb",
+      "city": "2",
+      "area": "Sector 29",
+      "totalRatingsString": "1000+ ratings",
+      "cloudinaryImageId": "1293a58ab5610f4c730c3d8cb6ac1b65",
+      "cuisines": [
+        "American"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 40000,
+      "costForTwoString": "₹400 FOR TWO",
+      "deliveryTime": 37,
+      "minDeliveryTime": 37,
+      "maxDeliveryTime": 37,
+      "slaString": "37 MINS",
+      "lastMileTravel": 7.599999904632568,
+      "slugs": {
+        "restaurant": "mcdonalds-hr-gurgaon-sec-29-huda-city",
+        "city": "gurgaon"
+      },
+      "cityState": "2",
+      "address": "HR Gurugram Sector- 29, SCO-36. Main Market, Sector-29, Gurugram-122001",
+      "locality": "Market Road",
+      "parentId": 630,
+      "unserviceable": false,
+      "veg": false,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "aggregatedDiscountInfo": {
+        "header": "50% off",
+        "shortDescriptionList": [
+          {
+            "meta": "50% off | Use WELCOME50",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "50% off up to ₹100 | Use code WELCOME50",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "aggregatedDiscountInfoV2": {
+        "header": "50% OFF",
+        "shortDescriptionList": [
+          {
+            "meta": "Use WELCOME50",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "50% off up to ₹100 | Use code WELCOME50",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          
+        ],
+        "totalFees": 0,
+        "message": "",
+        "title": "",
+        "amount": "",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 1,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "7.5 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "254005",
+        "deliveryTime": 37,
+        "minDeliveryTime": 37,
+        "maxDeliveryTime": 37,
+        "lastMileTravel": 7.599999904632568,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "IT_IS_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": false,
+      "avgRating": "4.2",
+      "totalRatings": 1000,
+      "new": false
+    },
+    "subtype": "basic"
+  },
+  {
+    "type": "restaurant",
+    "data": {
+      "type": "F",
+      "id": "625289",
+      "name": "Rollsking",
+      "uuid": "f27deb83-3c0b-4fc5-bc6a-bd486786361d",
+      "city": "2",
+      "area": "Palam Vihar",
+      "totalRatingsString": "100+ ratings",
+      "cloudinaryImageId": "df57m5tdoirinosrlsiy",
+      "cuisines": [
+        "North Indian",
+        "Fast Food",
+        "Beverages"
+      ],
+      "tags": [
+        
+      ],
+      "costForTwo": 25000,
+      "costForTwoString": "₹250 FOR TWO",
+      "deliveryTime": 30,
+      "minDeliveryTime": 30,
+      "maxDeliveryTime": 30,
+      "slaString": "30 MINS",
+      "lastMileTravel": 4.900000095367432,
+      "slugs": {
+        "restaurant": "rollsking-old-gurgaon-(zone-6)-old-gurgaon-(zone-6)-2",
+        "city": "gurgaon"
+      },
+      "cityState": "2",
+      "address": "BG-088A,GROUND FLOOR CONSCIENT ONE SECTOR-109 GURUGRAM HARYANA 122017",
+      "locality": "Conscient one mall",
+      "parentId": 4697,
+      "unserviceable": false,
+      "veg": false,
+      "select": false,
+      "favorite": false,
+      "tradeCampaignHeaders": [
+        
+      ],
+      "aggregatedDiscountInfo": {
+        "header": "50% off",
+        "shortDescriptionList": [
+          {
+            "meta": "50% off | Use WELCOME50",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "50% off up to ₹100 | Use code WELCOME50",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "aggregatedDiscountInfoV2": {
+        "header": "50% OFF",
+        "shortDescriptionList": [
+          {
+            "meta": "Use WELCOME50",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "descriptionList": [
+          {
+            "meta": "50% off up to ₹100 | Use code WELCOME50",
+            "discountType": "Percentage",
+            "operationType": "RESTAURANT"
+          }
+        ],
+        "subHeader": "",
+        "headerType": 0,
+        "superFreedel": ""
+      },
+      "ribbon": [
+        {
+          "type": "PROMOTED"
+        }
+      ],
+      "chain": [
+        
+      ],
+      "feeDetails": {
+        "fees": [
+          
+        ],
+        "totalFees": 0,
+        "message": "",
+        "title": "",
+        "amount": "",
+        "icon": ""
+      },
+      "availability": {
+        "opened": true,
+        "nextOpenMessage": "",
+        "nextCloseMessage": ""
+      },
+      "longDistanceEnabled": 0,
+      "rainMode": "NONE",
+      "thirdPartyAddress": false,
+      "thirdPartyVendor": "",
+      "adTrackingID": "cid=5871576~p=4~eid=00000186-4bde-b2fe-3ce0-21b700ad0400",
+      "badges": {
+        "imageBased": [
+          
+        ],
+        "textBased": [
+          
+        ],
+        "textExtendedBadges": [
+          
+        ]
+      },
+      "lastMileTravelString": "4.9 kms",
+      "hasSurge": false,
+      "sla": {
+        "restaurantId": "625288",
+        "deliveryTime": 30,
+        "minDeliveryTime": 30,
+        "maxDeliveryTime": 30,
+        "lastMileTravel": 4.900000095367432,
+        "lastMileDistance": 0,
+        "serviceability": "SERVICEABLE",
+        "rainMode": "NONE",
+        "longDistance": "NOT_LONG_DISTANCE",
+        "preferentialService": false,
+        "iconType": "EMPTY"
+      },
+      "promoted": true,
+      "avgRating": "4.3",
+      "totalRatings": 100,
+      "new": false
+    },
+    "subtype": "basic"
   }
 ];
 
